162a5b926074be737e396b8c7921e0b3d5e7c252
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-15 03:52:20 -0500

    Update to Hello Fresh Site

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const openLink = () => {
     let link = document.querySelector (".claimOfferBut");
     link.setAttribute('href')
 }
+
+// DOM Manipulation type - classlist.toggle//
+const imgToFoodItems = () => {
+    //1.target img (image) i.e the 1st class
+    let picture = document.querySelector('.img')
+    //2. target food i.e. the 2nd class
+    picture.classList.toggle('food')
+}
